dce31a253db234f39f0c88ec944d9e3413f5f5e4
parasojha258 <parasojha258@gmail.com>
2020-04-06 23:25:21 +0545

    image upload added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,77 +1,105 @@
+const multer = require('multer');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 
 const filterObj = (obj, ...allowedFields) => {
-	const newObj = {};
-	Object.keys(obj).forEach((el) => {
-		if (allowedFields.includes(el)) newObj[el] = obj[el];
-	});
-	return newObj;
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
 };
 
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split['/'][1];
+    cb(null, `user-${req.user.id}-${Date.now}.${ext}`);
+  },
+});
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Invalid image type', 400), false);
+  }
+};
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadsUserPhoto = upload.single('photo');
+
 exports.getAllUsers = async (req, res) => {
-	try {
-		const user = await User.find();
-		res.status(200).json({
-			status: 'success',
-			results: user.length,
-			user
-		});
-	} catch (err) {
-		res.status(404).json({
-			status: 'fail',
-			message: err
-		});
-	}
+  try {
+    const user = await User.find();
+    res.status(200).json({
+      status: 'success',
+      results: user.length,
+      user,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.updateMe = async (req, res, next) => {
-	try {
-		if (req.body.password || req.body.passwordConfirm) {
-			return next(new AppError('You cannot peform this action', 400));
-		}
-		const filteredBody = filterObj(req.body, 'name', 'email');
-		const user = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true });
-		res.status(200).json({
-			status: 'success',
-			user
-		});
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    if (req.body.password || req.body.passwordConfirm) {
+      return next(new AppError('You cannot peform this action', 400));
+    }
+    const filteredBody = filterObj(req.body, 'name', 'email');
+    if (req.file) filteredBody.photo = req.file.filename;
+    const user = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      user,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 exports.deleteMe = async (req, res, next) => {
-	try {
-		await User.findByIdAndUpdate(req.user.id, { active: false });
-		res.status(204).json({
-			status: 'success',
-			data: null
-		});
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    await User.findByIdAndUpdate(req.user.id, { active: false });
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 exports.createUser = (req, res) => {
-	res.status(500).json({
-		status: 'fail'
-	});
+  res.status(500).json({
+    status: 'fail',
+  });
 };
 
 exports.getUser = (req, res) => {
-	res.status(500).json({
-		status: 'fail'
-	});
+  res.status(500).json({
+    status: 'fail',
+  });
 };
 
 exports.updateUser = (req, res) => {
-	res.status(500).json({
-		status: 'fail'
-	});
+  res.status(500).json({
+    status: 'fail',
+  });
 };
 
 exports.deleteUser = (req, res) => {
-	res.status(500).json({
-		status: 'fail'
-	});
+  res.status(500).json({
+    status: 'fail',
+  });
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,15 +8,32 @@ router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetpassword/:token', authController.resetPassword);
-router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
-router.patch('/updateMe', authController.protect, userController.updateMe);
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword
+);
+router.patch(
+  '/updateMe',
+  userController.uploadsUserPhoto,
+  authController.protect,
+  userController.updateMe
+);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 router
-	.route('/')
-	.get(authController.protect, authController.restrictTo('admin'), userController.getAllUsers)
-	.post(authController.protect, userController.createUser);
+  .route('/')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin'),
+    userController.getAllUsers
+  )
+  .post(authController.protect, userController.createUser);
 
-router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
